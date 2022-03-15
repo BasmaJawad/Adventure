e7775af296600e66b8f96ca52d6e6439148652ad
BasmaJawad <basma.jawad02@gmail.com>
2022-03-15 13:02:58 +0100

    descriptions

---
+++ del1/RoomDescriptions.java
@@ -0,0 +1,35 @@
+package del1;
+
+public class RoomDescriptions {
+
+        String room1(){
+            return "the room is filled with tomatoes";
+        }
+
+        String room2(){
+            return "the room is filled with potatoes";
+        }
+        String room3(){
+            return "the room is filled with onions";
+        }
+        String room4(){
+            return "the room is filled with chocolate ";
+        }
+        String room5(){
+            return "The room is filled with berries ";
+        }
+        String room6(){
+            return "The room is filled with dogs";
+        }
+        String room7(){
+            return "The room is filled with monsters";
+        }
+        String room8(){
+            return "The room is filled with blood";
+        }
+        String room9(){
+            return "The room is filled with dead bodies";
+        }
+
+
+    }
