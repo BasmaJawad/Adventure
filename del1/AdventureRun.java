c4293d318b0d1d31f49ba858fc15e59d3dc19dce
Marcus-K-Thorsen <marc303i@stud.kea.dk>
2022-03-19 20:04:32 +0100

    Indsat noter i AdventureRun og GameEngine

--- del1/AdventureRun.java
+++ del1/AdventureRun.java
@@ -3,7 +3,6 @@ package del1;
 public class AdventureRun {
 
   public static void main(String[] args) {
-
-    new GameEngine().startGame();
+    new GameEngine().startGame(); // Påbegynder en 'new GameEngine' class og kalder dets Metode 'sterGame()'
   }
 }
