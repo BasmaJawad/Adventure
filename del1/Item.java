fe8fed7787237769d20ee461baa44be4241e8280
Marcus-K-Thorsen <marc303i@stud.kea.dk>
2022-03-28 13:54:46 +0200

    Ændret rundt på Item Class, så der ikke er nogen Enum ItemType

--- del1/Item.java
+++ del1/Item.java
@@ -3,35 +3,15 @@ package del1;
 public class Item {
     private String itemNameShort;
     private String itemNameLong;
-    private ItemType itemType;
 
 
     Item(String itemNameShort, String itemNameLong) {
         this.itemNameShort = itemNameShort;
         this.itemNameLong = itemNameLong;
-        //setItemType(itemNameShort);
     }
 
-    Item(String itemNameShort, String itemNameLong, String itemType) {
-        this.itemNameShort = itemNameShort;
-        this.itemNameLong = itemNameLong;
-        //setItemType(itemType);
-    }
 
-    private void setItemType(String itemType) { // Metode der tager et String argument, det indikere en type item
-        itemType = itemType.toLowerCase(); // String argumentet bliver sat til at være i lower case og gemmes i sig selv
-        if (itemType.contains("ye")) { // Hvis dette String Argument indeholder "ye" så vil:
-            this.itemType = ItemType.YELLOW; // sat til at være gul
-        } else if (itemType.contains("bl")) { // og hvis det indeholder "bl"
-            this.itemType = ItemType.BLUE; // så er kombinationen blå
-        } else if (itemType.contains("re")) { // også videre
-            this.itemType = ItemType.RED;
-        } else { // Men hvis der ikke er nogen af de passene fraser i String argumentet, så vil:
-            this.itemType = ItemType.NONE; // hvis den er denne item type så er det en speciel winner item
-            itemNameShort = "Holy Grail";
-            itemNameLong = "The Holy Grail";
-        }
-    }
+
 
     // Getters
     public String getItemNameShort() {
@@ -40,10 +20,6 @@ public class Item {
 
     public String getItemNameLong() {
         return itemNameLong;
-    }
-
-    public ItemType getItemType() {
-        return itemType;
     }
 }
 
